d7bb1e8bf27b57cfbbfade416bb17240b86dea39
st086736 <90981919+st086736@users.noreply.github.com>
2021-09-19 23:37:40 +0300

    Add files via upload

---
+++ 2021.09.18-Lesson 1/Project8/Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c, x, y, z;
+	cin >> a >> b >> c >> x >> y >> z;
+	cout << (x - a) * 3600 + (y - b) * 60 + (z - c) << endl;
+
+	return 0;
+
+}
